a7ce37537c90f64171820bf77ebdd2f068d4584d
jiangtao <jtaobox@126.com>
2018-03-20 21:47:55 +0800

    重新整理代码，采用window.getSelection获取选中文字、position；

---
+++ js/lib/Tip.js
@@ -0,0 +1,73 @@
+class Tip {
+  constructor() {
+    // 初始化tip 并追加至body
+    this.tip = this.createTip();
+    this.tipStyle = this.tip.style;
+    this.tipContainer = this.tip.querySelector('.tip-content');
+    window.document.body.append(this.tip);
+  }
+  /**
+   * 显示tip
+   * @param {object} param0 
+   */
+  show({ resList, rect }) {
+    const eleArr = resList.map(item => {
+      item.pre || (item.pre = "");
+      let p = document.createElement('p');
+      p.innerText = `${item.pre} ${item.cont}`;
+      return p.outerHTML;
+    });
+    this.insertToTip(eleArr);
+    this.moveToPos(rect);
+  }
+  /**
+   * 隐藏tip
+   */
+  hide() {
+    if (this.tipStyle.display != 'none')
+      this.tipStyle.display = 'none'; 
+  }
+  /**
+   * 向tip填充数据
+   * @param {array} eleArr 
+   */
+  insertToTip(eleArr) {
+    this.tipContainer.innerHTML = eleArr.join()
+  }
+  /**
+   * 移动tip
+   * @param {object} rect 
+   */
+  moveToPos(rect) {
+    this.modifyTipPosition(rect);
+  }
+  /**
+   * 修改tip位置
+   * @param {object} param0 
+   */
+  modifyTipPosition({top, left, height}) {
+    if (this.tipStyle.display == 'none') 
+      this.tipStyle.display = 'block';
+    this.tipStyle.top = top + height + 8 + 'px';
+    this.tipStyle.left = left + 'px';
+  }
+  /**
+   * 创建tip dom
+   */
+  createTip() {
+    // 创建dom
+    let container = `
+    <div class="tip-container">
+        <div class="tip-content">
+        </div>
+        <div class="tip-arrow">
+            <i></i>
+            <em></em>
+        </div>
+    </div>
+    `;
+    return $(container).addClass('translateX')[0];
+  }
+
+ 
+}
--- js/main.js
+++ js/main.js
@@ -1,120 +1,36 @@
-$(function () {
-    let tipContent = null;
-    let translateX = null;
-    $(document.body).on("mouseup", function () {
-        if (tipContent) tipContent.innerHTML = "";
-        var select = document.getSelection();
-        var selectText = select.toString();
-        if (!selectText) {
-            translateX && (translateX.style.display = "none");
-            return;
-        }
-        translateX && (translateX.style.display = "block");
-        markSelection(selectText);
-
-        $.get("https://sp1.baidu.com/5b11fzupBgM18t7jm9iCKT-xh_/sensearch/selecttext", {
-            q: selectText,
-            _: new Date().getTime()
-        }, function (result) {
-            // 填充结果
-            var res = result.data.result;
-            if (!res) return;
-            if (!Array.isArray(res)) {
-                res = [{
-                    pre: "",
-                    cont: res
-                }];
-            }
-            for (var i = 0; i < res.length; i++) {
-                var item = res[i];
-                var p = document.createElement("p");
-                p.innerHTML = item.pre + " " + item.cont;
-                tipContent.appendChild(p);
-            }
+const tip = new Tip();
+// 监听鼠标抬起 显示tip
+document.body.addEventListener('mouseup', () => {
+    // 获取选中文字 以及位置、宽高等信息
+    let { rect, seleStr } = getSelectPos();
+    if (!seleStr) return tip.hide();
+    $.get("https://sp1.baidu.com/5b11fzupBgM18t7jm9iCKT-xh_/sensearch/selecttext", {
+        q: seleStr,
+        _: new Date().getTime()
+    }, function (result) {
+        if (result.errno > 0) return;
+        let resList = result.data.result;
+        if (!Array.isArray(resList)) resList = [{
+            pre: "",
+            cont: resList
+        }]
+        tip.show({
+            resList, rect
         });
-    });
-    window.onscroll = function() {
-        tipContent && (tipContent.innerHTML = "");
-        translateX && 
-        (translateX.style.display == "block") &&
-        (translateX.style.display = "none");
+    })
+
+});
+// 当滑动时隐藏tip
+document.addEventListener('scroll', () => {
+    tip.hide();
+});
+
+function getSelectPos() {
+    let selection = window.getSelection();
+    if (!selection.rangeCount) return;
+    let range = selection.getRangeAt(0);
+    return {
+        rect: range.getBoundingClientRect(),
+        seleStr: range.toString()
     };
-    var markSelection = (function () {
-
-        var markerEl,
-            markerId = "sel_" +
-            new Date().getTime() + "_" +
-            Math.random().toString().substr(2);
-
-        var selectionEl;
-
-        // 创建dom
-        var container = `
-            <div class="tip-container">
-                <div class="tip-content">
-                </div>
-                <div class="tip-arrow">
-                    <i></i>
-                    <em></em>
-                </div>
-            </div>
-        `;
-
-        selectionEl = document.createElement("div");
-        selectionEl.classList.add("translateX");
-        selectionEl.innerHTML = container;
-
-        return function (selectText) {
-            var sel, range;
-
-            sel = window.getSelection();
-
-            if (sel.getRangeAt) {
-                range = sel.getRangeAt(0).cloneRange();
-            } else {
-                // Older WebKit doesn't have getRangeAt
-                range = document.createRange();
-                range.setStart(sel.anchorNode, sel.anchorOffset);
-                range.setEnd(sel.focusNode, sel.focusOffset);
-
-                // Handle the case when the selection was selected backwards (from the end to the start in the
-                // document)
-                if (range.collapsed !== sel.isCollapsed) {
-                    range.setStart(sel.focusNode, sel.focusOffset);
-                    range.setEnd(sel.anchorNode, sel.anchorOffset);
-                }
-            }
-
-            range.collapse(false);
-
-            markerEl = document.createElement("span");
-            markerEl.id = markerId;
-            markerEl.appendChild(document.createTextNode(selectText));
-            range.insertNode(markerEl);
-            var markerElHeigth = markerEl.getBoundingClientRect().height;
-            var markerElWidth = markerEl.getBoundingClientRect().width;
-
-            if (markerEl) {
-                // Lazily create element to be placed next to the selection
-                document.body.appendChild(selectionEl);
-
-                !tipContent && (tipContent = document.querySelector(".tip-content"));
-                !translateX && (translateX = document.querySelector(".translateX"));
-
-                // Find markerEl position http://www.quirksmode.org/js/findpos.html
-                var obj = markerEl;
-                var left = 0,
-                    top = 0;
-                do {
-                    left += obj.offsetLeft;
-                    top += obj.offsetTop;
-                } while (obj = obj.offsetParent);
-                // Move the button into place.
-                // Substitute your jQuery stuff in here
-                selectionEl.style.left = left - markerElWidth + "px";
-                selectionEl.style.top = top + markerElHeigth + 8 + "px";
-                markerEl.parentNode.removeChild(markerEl);
-            }
-        };
-    })();
-})
+}
